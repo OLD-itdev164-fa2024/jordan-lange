f0a873f1b0792c1bff302b6057d80cbdce975b52
JordanL <langej11@gmatc.edu>
2023-10-08 05:19:31 -0500

    completed activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Jordan Lange`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Jordan Lange`,
+      company: `Great Gatsby Blogs INC`,
+      address: ` PO Box 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
